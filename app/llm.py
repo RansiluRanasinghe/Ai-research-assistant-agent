872a19873e7884332097ba06d19b9d2ba3d5a699
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-03-31 19:53:25 +0530

    Fixed  response not generating issue

--- app/llm.py
+++ app/llm.py
@@ -8,12 +8,12 @@ load_dotenv()
 
 class LLMService:
 
-    def __init__(self, model_name = "google/flan-t5-small", use_api_fallback=True):
+    def __init__(self, model_name = "distilgpt2", use_api_fallback=True):
         self.modle_name = model_name
         self.use_api_fallback = use_api_fallback
 
         try:
-            self.pipeline = pipeline("text-generation", model=self.modle_name, device=1)
+            self.pipeline = pipeline("text-generation", model=self.modle_name, device= -1)
 
             self.local_available = True
         except Exception as e:
@@ -25,17 +25,19 @@ class LLMService:
             self.headers = {"Authorization": f"Bearer {self.api_token}"}
 
     def generate(self, prompt, max_new_tokens=500):
+
+        formatted_prompt = f"Answer the following question clearly:\n{prompt}"
         
         if self.local_available:
             try:
-                response = self.pipeline(prompt, max_new_tokens=max_new_tokens)
+                response = self.pipeline(formatted_prompt, max_new_tokens=max_new_tokens)
                 return response[0]['generated_text']
             except Exception as e:
                 print("Local failed, switching to API:", e)
 
         if self.use_api_fallback:
             payload = payload = {
-                "inputs": prompt,
+                "inputs": formatted_prompt,
                 "parameters": {"max_new_tokens": max_new_tokens}
             }
 
