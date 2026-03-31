3a7d0bfef5014cae397d48335af7b9a56daab9c4
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-03-31 18:40:17 +0530

    Added the feature to generate responses from the model

--- app/llm.py
+++ app/llm.py
@@ -4,3 +4,7 @@ class LLMService:
 
     def __init__(self, model_name = "google/flan-t5-base"):
         self.pipeline = pipeline("text2text-generation", model=model_name, device=1)
+
+    def generate(self, prompt, max_new_tokens=500):
+        response = self.pipeline(prompt, max_new_tokens=max_new_tokens)
+        return response[0]['generated_text']
