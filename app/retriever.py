10cda88349a40aeac284f58dd7ae01669116f279
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-04-01 20:16:41 +0530

    Added feature to search similar words

--- app/retriever.py
+++ app/retriever.py
@@ -20,5 +20,14 @@ class VectorStore:
             return
 
         vectors = self.emb_model.encode(doc_texts)
-        self.index.add(vectors.astype('float32'))
-        self.chunks.extend(doc_texts)    
+        self.index.add(vectors.astype(np.float32))
+        self.chunks.extend(doc_texts)
+
+    def search(self, query, top_k=3):
+
+        query_vec = self.emb_model.encode(query).astype(np.float32).reshape(1, -1)
+        distances, indices = self.index.search(query_vec, min(top_k, len(self.chunks)))
+
+        results = [(self.chunks[i], distances[0][j]) for j, i in enumerate(indices[0])]
+
+        return results
