aa1c64afd24ef5040affaec3b780a2f1b2befa2a
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-04-01 18:45:46 +0530

    Feature added document words chunking

--- app/utils.py
+++ app/utils.py
@@ -20,4 +20,18 @@ def load_document(directory):
                 docs.append(f.read())
 
 
-    return docs            
+    return docs
+
+def chunk_text(text, chunk_size=1000, overlap=50):
+
+    words = text.split()
+    chunks = []
+    start = 0
+
+    while start < len(words):
+        end = start + chunk_size
+        chunk_words = words[start:end]
+        chunks.append(" ".join(chunk_words))
+        start += chunk_size - overlap
+
+    return chunks    
