c22a16cbc66094e59377e01087e70ceb727114d5
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-04-01 19:17:43 +0530

    Added encoding feature from encoder model

--- app/embeddings.py
+++ app/embeddings.py
@@ -5,4 +5,11 @@ class EmbeddingModel:
 
     def __init__(self, model_name="all-MiniLM-L6-v2"):
         self.model = SentenceTransformer(model_name)
+
+    def encode(self, texts, batch_size=32):
+
+        if isinstance(texts, str):
+            texts = [texts]
+
+        return self.model.encode(texts, batch_size=batch_size, show_progress_bar=False)    
         
