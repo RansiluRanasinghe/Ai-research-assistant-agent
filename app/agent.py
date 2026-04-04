34c69602b327a128f9253285f130e9f038b9f023
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-04-04 15:46:36 +0530

    feat: added run function to generate answers for user queries

--- app/agent.py
+++ app/agent.py
@@ -6,3 +6,13 @@ class Agent:
     def __init__(self, rag_pipeline, llm_service):
         self.rag = rag_pipeline
         self.llm_service = llm_service
+
+    def run(self, query):
+
+        research_keywords = ["research", "paper", "document", "study", "according to", "what does the"]
+
+        if any(kw in query.lower() for kw in research_keywords):
+            return self.rag.generate_answer(query)
+        else:
+            prompt = f"Answer the following question concisely:\n{query}"
+            return self.llm_service.generate(prompt, max_new_tokens=100)    
