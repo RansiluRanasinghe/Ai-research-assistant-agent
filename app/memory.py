9145ce550e631a4cd8f7fef3c4b8ebfe25b16cc1
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-04-04 16:39:54 +0530

    feat: implemented add history feature

--- app/memory.py
+++ app/memory.py
@@ -3,3 +3,9 @@ class Memory:
     def __init__(self, max_history=5):
         self.max_history = max_history
         self.history = []
+
+    def add(self, user_input, assistant_output):
+
+        self.history.append((user_input, assistant_output))
+        if len(self.history) > self.max_history:
+            self.history.pop(0)  
