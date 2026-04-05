67c1b7047d64150e5e895893f1084d5687f789c8
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-04-05 15:47:27 +0530

    feat:initialized the UI Configurations

---
+++ app/web_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import os
+from pathlib import Path
+from llm import LLMService
+from embeddings import EmbeddingModel
+from retriever import RAGPipeline, VectorStore
+from agent import Agent
+from memory import Memory
+from utils import load_document, chunks_text
+
+
+st.set_page_config(page_title="AI Research Assistant", page_icon="🤖", layout="wide")
+st.title("AI Research Assistant")
+
